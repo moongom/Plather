4643562165e0b6773f435625227ee12ed826e7bc
iceCreamParlor <heejjeeh@gmail.com>
2018-12-22 17:12:17 +0900

    push

--- frontend/theorbit/src/app/global.ts
+++ frontend/theorbit/src/app/global.ts
@@ -1,6 +1,6 @@
 
 'use strict';
 
-export const endpoint='http://www.greedyturtles.com:3000/api/v1';
+export const endpoint='http://localhost:3000/api/v1';
 // export const endpoint='http://192.168.0.24:3000/api/v1';
 export const useSampleData = false;
